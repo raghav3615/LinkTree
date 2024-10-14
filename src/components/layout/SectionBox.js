adf1095dec45e6e39e6f99fec2730f1bdf242da0
raghav3615 <dadhichraghav896@gmail.com>
2024-10-14 12:41:29 +0530

    code update

--- src/components/layout/SectionBox.js
+++
@@ -1,7 +0,0 @@
-export default function SectionBox({children}) {
-  return (
-    <div className="bg-white m-8 p-4 shadow">
-      {children}
-    </div>
-  );
-}
--- src/models/Event.js
+++
@@ -1,9 +0,0 @@
-import {model, models, Schema} from "mongoose";
-
-const EventSchema = new Schema({
-  type: String, // click or view
-  page: String, // for example "dawid"
-  uri: String, // /dawid | https://
-}, {timestamps: true});
-
-export const Event = models?.Event || model('Event', EventSchema);
